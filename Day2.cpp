bc76945e7966439a7af4cff9b04fa68c2666dcae
Dhananjoy Ghosh <dhananjoyghohsofficial@gmail.com>
2024-04-01 10:39:07 +0530

    Day3

--- Day2.cpp
+++ Day2.cpp
@@ -4,7 +4,8 @@
 // step2 : Call the function one with parameter( arr, st=0, end= d-1) which reverses the part1 which is 0 to d-1 ;
 //step3 : call the function one which parameter (arr, st= d , end = n) which revereses the part2 which is d to n ;
 //step4 : call the function one which parameter (arr, st = 0 , end=n) which reverses the part3 which is 0 to n-1 ;
-
+//TC- O(log(n)+O(log(n/2))+O(log(n/2))) ~= O(log(n))
+//SC- O(1)
 
 void reverse(int arr[], int st, int end){
     while(st<end){
---
+++ Day3.cpp
@@ -0,0 +1,47 @@
+//Question1 : Maximum SubArray Leetcode 53
+//1st Approach : Using two loops 
+// for i in range(n) and j from i to n for getting all the contiguous subarrays 
+// store the sum of all the possible subarrays and compare with them with maxi which is storing the maximum sum from all the possible subarrays
+// TC - O(n^2)
+//SC - O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) { 
+        int n= nums.size();
+        int maxi = INT_MIN;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                maxi =max(maxi,sum);
+            }
+            
+        }
+        return maxi;
+    }
+};
+
+//2nd Approach - Kadane's Algorithm
+//initialise the maxi = nums[0] & sum = 0;
+//step 1 : sum +=nums[i];
+//step2  : maxi= max(maxi,sum);
+// if(sum <0) sum =0;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) { 
+        int n= nums.size();
+        int maxi = nums[0];
+        int sum = 0;
+
+        for(int i = 0 ;i<n;i++){
+            sum +=nums[i];
+            maxi=max(maxi,sum);
+            if(sum <0){
+                sum =0;
+            }
+        }   
+        
+        return maxi;
+    }
+};
