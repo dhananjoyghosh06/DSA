408e1c0a7e1fdbec5d5d7a40ba31258cd1dcb32b
Dhananjoy Ghosh <dhananjoyghohsofficial@gmail.com>
2024-04-12 14:05:07 +0530

    Day10

--- Day10.cpp
+++ Day10.cpp
@@ -1,3 +1,5 @@
+//Subsequesnces of a string
+//Method used - Recursion 
 #include <bits/stdc++.h>
 void helper(string str, vector<string>&ans, string op, int i)
 {
@@ -27,3 +29,37 @@ vector<string> subsequences(string str){
 	return ans;
 	
 }
+
+//String Permutations 
+//Mobile keypad number letter combinations 
+//Method used - Recursion + Backtracking
+class Solution {
+public:
+    void getAns(string digits, vector<string>&ans,string op, string mapp[], int i ){
+        if(i>=digits.length()){
+            ans.push_back(op);
+            return ;
+        }
+        //find the i'th number
+        int number = digits[i]-'0';
+        //find the map value corresponding to the mapp
+        string value = mapp[number];
+        //traverse the value
+        for(int j=0;j<value.length();j++){
+            op.push_back(value[j]);
+            getAns(digits, ans, op, mapp, i+1);
+            op.pop_back();
+        }
+
+
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length()==0) return ans;
+        string op = "";
+        string mapp[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        getAns(digits, ans, op, mapp, 0);
+
+        return ans;
+    }
+};
