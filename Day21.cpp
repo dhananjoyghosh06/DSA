fecd82bdbf111651f4aaefd3c62ac09f6f13d1ca
Dhananjoy Ghosh <dhananjoyghohsofficial@gmail.com>
2024-05-01 10:34:25 +0530

    Day21

---
+++ Day21.cpp
@@ -0,0 +1,43 @@
+//Q1. 1st approach - Using queue
+// TC- O(n)
+//SC-O(N^2)
+
+#include<bits/stdc++.h>
+using namespace std;
+bool solve(string s, string goal){
+    if(s.length() != goal.length()) return false;
+    
+    // q1 push
+    queue<int>q1;
+    queue<int>q2;
+    
+    for(int i =0;i<s.length();i++){
+        q1.push(s[i]);
+        q2.push(goal[i]);
+    }
+    // rotate
+    int k = 0;
+    while(k<s.length()){
+        char p = q1.front();
+        q1.push(p);
+        q1.pop();
+        
+        
+        if(q1==q2) return true;
+        
+        k++;
+    }
+    
+    return false;
+    
+}
+int main(){
+    string s, goal;
+    cin>>s>>goal;
+    
+    cout<<solve(s,goal);
+}
+
+//Q2 > Method used - Concatenation method 
+// TC- O(n)
+//SC-O(1)
